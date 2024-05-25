a55e6269db169884bde68e432f9126c7085173d9
Pritesh Mishra <priteshmishra2125@gmail.com>
2024-05-25 21:58:37 +0530

    frontend test

--- frontend-todo/src/App.jsx
+++ frontend-todo/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CreateTodo } from './components/CreateTodo'
 
 function App() {
 
   return (
     <div>
-        Hi there
+        <CreateTodo/>
     </div>
   )
 }
